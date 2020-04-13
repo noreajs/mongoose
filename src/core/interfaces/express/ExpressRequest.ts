aeb8a7e430bf48f4ee70e047d63a008b587fa8c9
Arnold LAMBOU <lambouarnold@gmail.com>
2020-04-13 20:27:08 +0200

    ExpressRequest

--- src/core/interfaces/express/ExpressRequest.ts
+++ src/core/interfaces/express/ExpressRequest.ts
@@ -1,9 +1,7 @@
 import { Request } from "express";
 import IUser from "../../../interfaces/IUser";
-import ICountryIpLookup from "../../../interfaces/ICountryIpLookup";
 
 
 export default interface ExpressRequest extends Request<any> {
-    user: IUser,
-    countryLookup: ICountryIpLookup
+    user: IUser
 }
