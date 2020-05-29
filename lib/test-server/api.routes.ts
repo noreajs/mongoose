4181e0c610e97ab40cfd8379c42ec4f1c28d4968
Arnold L <lambouarnold@gmail.com>
2020-05-29 11:41:46 +0200

    test server updated

--- lib/test-server/api.routes.ts
+++ lib/test-server/api.routes.ts
@@ -1,6 +1,6 @@
 import { NoreaAppRoutes } from "@noreajs/core";
 import { Application, Request, Response } from "express";
-import taskModel from "./test-server/models/task.model";
+import taskModel from "./models/task.model";
 
 export default new NoreaAppRoutes({
   routes(app: Application): void {
@@ -19,9 +19,11 @@ export default new NoreaAppRoutes({
     });
 
     app.get("/tasks", async (request: Request, response: Response) => {
-      const r = await taskModel.findOne({});
-      r?.hello('comment tu va?');
-      response.send(r);
+      const r = await taskModel.findById("5eca5dfcb03d800d009fb9b0");
+      if(r){
+        r.hello('comment tu va?');
+      }
+      return response.send(r);
     });
 
     app.route("/tasks").post([
