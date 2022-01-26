28a8f1f7008fe5c52e3e9c03e28b41561599e1cf
lambou <lambouarnold@gmail.com>
2022-01-26 19:56:35 +0100

    You can't call updateInitConfig method when the app already started Bug fixed

--- lib/core/MongodbContext.ts
+++ lib/core/MongodbContext.ts
@@ -1,4 +1,4 @@
-import { connection, connect, Connection } from "mongoose";
+import { connection, connect, Connection, ConnectOptions } from "mongoose";
 
 export type MongoDBContextParams = {
   /**
@@ -9,7 +9,7 @@ export type MongoDBContextParams = {
   /**
    * MongoDB connection options
    */
-  options?: any;
+  options?: ConnectOptions;
 
   /**
    * Method to be executed when the connection to MongoDB has been etablished
@@ -44,66 +44,102 @@ class MongoDBContext {
    *
    * @param params Mongodb context params
    */
-  static async init(params: MongoDBContextParams): Promise<void> {
-    // listen connection
-    const db = connection;
+  static init(params: MongoDBContextParams): Promise<void> {
+    return new Promise<void>(async (resolve, reject) => {
+      // listen connection
+      const db = connection;
 
-    /**
-     * Catch error
-     */
-    db.on("error", async function () {
-      console.error.bind(console, "MongoDB connection error:");
-      // after connection
-      if (params.onError) {
-        await params.onError(db);
-      }
-    });
+      /**
+       * Catch error
+       */
+      db.on("error", async function () {
+        console.error.bind(console, "MongoDB connection error:");
+        // after connection
+        if (params.onError) {
+          await params.onError(db);
+        }
 
-    /**
-     * Once connection openned
-     */
-    db.once("open", async function () {
+        // initialization failed
+        reject();
+      });
+
+      // /**
+      //  * Once connection openned
+      //  */
+      // db.once("open", async function () {
+      //   console.log("mongodb opened");
+      // });
+
       try {
-        for (const modelName of MongoDBContext.syncIndexes.keys()) {
-          const options = MongoDBContext.syncIndexes.get(modelName);
-          if (options) {
-            if (options.enabled !== false) {
-              if (options.callback) {
-                db.models[modelName].syncIndexes(
-                  options.options ?? null,
-                  options.callback
-                );
-              } else {
-                db.models[modelName].syncIndexes(options.options);
+        // trigger connection
+        await connect(
+          params.connectionUrl,
+          params.options ?? {},
+          async (error) => {
+            /**
+             * Error not defined
+             */
+            if (error !== null && error !== undefined) {
+              if (params.onError) {
+                try {
+                  await params.onError(db, error);
+                } catch (ignoredError) {
+                  // initialization failed
+                  reject(ignoredError);
+                }
               }
+
+              // initialization failed
+              reject(error);
+            } else {
+              /**
+               * Indexes synchronization
+               * --------------------------
+               */
+              try {
+                for (const modelName of MongoDBContext.syncIndexes.keys()) {
+                  const options = MongoDBContext.syncIndexes.get(modelName);
+                  if (options) {
+                    if (options.enabled !== false) {
+                      if (options.callback) {
+                        db.models[modelName].syncIndexes(
+                          options.options ?? null,
+                          options.callback
+                        );
+                      } else {
+                        db.models[modelName].syncIndexes(options.options);
+                      }
+                    }
+                  }
+                }
+              } catch (error) {
+                console.error("Failed to synchronize indexes", error);
+              }
+
+              /**
+               * Connection callback
+               * -------------------------
+               */
+              if (params.onConnect) {
+                await params.onConnect(db);
+              }
+
+              // continue
+              resolve();
             }
           }
-        }
+        );
       } catch (error) {
-        console.error("Failed to synchronize indexes", error);
-      }
-      // after connection
-      if (params.onConnect) {
-        await params.onConnect(db);
-      }
-    });
-
-    try {
-      // trigger connection
-      await connect(params.connectionUrl, params.options, (error) => {
         if (params.onError) {
           try {
-            params.onError(db, error);
+            await params.onError(db, error);
           } catch (ignoredError) {}
         }
-      });
-    } catch (error) {
-      if (params.onError) {
-        try {
-          await params.onError(db, error);
-        } catch (ignoredError) {}
+
+        // fail
+        reject(error);
       }
-    }
+    });
   }
 }
 
--- lib/test-server/run.ts
+++ lib/test-server/run.ts
@@ -1,28 +1,37 @@
-import { NoreaApp } from "@noreajs/core";
-import apiRoutes from "./api.routes";
+import { NoreaBootstrap } from "@noreajs/core";
+import connectMongo from "connect-mongo";
 import MongoDBContext from "../core/MongoDBContext";
+import apiRoutes from "./api.routes";
 
 /**
  * Create a new NoreaJs App
  */
-const app = new NoreaApp(apiRoutes, {
+const api = new NoreaBootstrap(apiRoutes, {
   forceHttps: false,
-  beforeStart: (app) => {
-    // Get MongoDB Instance
-    MongoDBContext.init({
-      connectionUrl: `mongodb://127.0.0.1:27017/noreajs_mongoose_db?compressors=zlib`,
-      onConnect: () => {
-        console.log("we are connected on mongodb");
-      },
-    });
-  },
-  afterStart: (app, server, port) => {
-    console.log("@noreajs/mongoose test server");
-    console.log("The api is running on port", port);
-  },
+});
+
+api.beforeInit(async (app) => {
+  // Get MongoDB Instance
+  await MongoDBContext.init({
+    connectionUrl: `mongodb://127.0.0.1:27017/noreajs_mongoose_db?compressors=zlib`,
+    onConnect: (connection) => {
+      console.log("we are connected on mongodb");
+      api.updateInitConfig({
+        sessionOptions: {
+          secret: "babayaga",
+          store: connectMongo.create({
+            client: connection.getClient() as any,
+          }),
+        },
+      });
+      console.log("setting updated");
+    },
+  });
+
+  console.log("finished");
 });
 
 /**
  * Start your app
  */
-app.start(3000);
+api.start(3000);
